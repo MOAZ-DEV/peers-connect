27b7641d8c0f488e30f876b24a30f0c2ed3d41f6
MoaazAllaElden <moaz.dev@yahoo.com>
2024-11-03 05:23:08 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { UserRegist } from "@/components/forms/UserCase";
 const Home = () => {
 
   return (
-    <div className="min-h-screen flex flex-col gap-12 items-center justify-center">
+    <div className="min-h-screen flex flex-col gap-12 items-center justify-center py-9">
       <UserRegist />
     </div>
   );
