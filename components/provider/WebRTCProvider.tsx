cd8ced9389965a9049b12a0ae081d4dae7d894ff
MoaazAllaElden <moaz.dev@yahoo.com>
2024-11-19 16:17:03 +0200

    fix

--- components/provider/WebRTCProvider.tsx
+++ components/provider/WebRTCProvider.tsx
@@ -36,8 +36,17 @@ import React, {
       if (typeof window !== "undefined" && "RTCPeerConnection" in window) {
         const pc = new RTCPeerConnection({
           iceServers: [
-            { urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"] },
-          ],
+            { urls: "stun:stun.l.google.com:19302" },
+            { urls: "stun:stun.l.google.com:5349" },
+            { urls: "stun:stun1.l.google.com:3478" },
+            { urls: "stun:stun1.l.google.com:5349" },
+            { urls: "stun:stun2.l.google.com:19302" },
+            { urls: "stun:stun2.l.google.com:5349" },
+            { urls: "stun:stun3.l.google.com:3478" },
+            { urls: "stun:stun3.l.google.com:5349" },
+            { urls: "stun:stun4.l.google.com:19302" },
+            { urls: "stun:stun4.l.google.com:5349" }
+                  ],
           iceCandidatePoolSize: 10,
         });
         PcRef.current = pc;
