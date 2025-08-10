d56ecb70c07a38f0f060ba118ba98bc4f5ed7222
Moaaz Alla Elden <moaz.dev@yahoo.com>
2025-08-10 14:04:02 +0300

    Update use-firestore.ts

--- hooks/use-firestore.ts
+++ hooks/use-firestore.ts
@@ -3,13 +3,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD5RL79Q5HYy2s_RacsiAuFotPz7PzTd0c",
-    authDomain: "klam-p2p.firebaseapp.com",
-    projectId: "klam-p2p",
-    storageBucket: "klam-p2p.firebasestorage.app",
-    messagingSenderId: "4158287384",
-    appId: "1:4158287384:web:0822f586b2897cb29e9d6d",
-    measurementId: "G-X7G63RGGTZ"
+  apiKey: "AIzaSyD5RL79Q5HYy2s_RacsiAuFotPz7PzTd0c",
+  authDomain: "klam-p2p.firebaseapp.com",
+  projectId: "klam-p2p",
+  storageBucket: "klam-p2p.firebasestorage.app",
+  messagingSenderId: "4158287384",
+  appId: "1:4158287384:web:0822f586b2897cb29e9d6d",
+  measurementId: "G-X7G63RGGTZ"
 };
 
 const initializeFirebase = () => {
